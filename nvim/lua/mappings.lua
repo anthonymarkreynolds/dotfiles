499c29d00b5a42bf1cd883de6f2657c04556f350
anthonymarkreynolds <anthonymarkreynolds@outlook.com>
2023-01-05 13:41:03 +1000

    init commit

---
+++ nvim/lua/mappings.lua
@@ -0,0 +1,9 @@
+-- Change Leader to Space
+vim.g.mapleader = "<Space>"
+
+-- use ctr to navigate windows
+local opts = { noremap = true, silent = true }
+vim.keymap.set("n", "<C-h>", "<C-w>h", opts)
+vim.keymap.set("n", "<C-j>", "<C-w>j", opts)
+vim.keymap.set("n", "<C-k>", "<C-w>k", opts)
+vim.keymap.set("n", "<C-l>", "<C-w>l", opts)
